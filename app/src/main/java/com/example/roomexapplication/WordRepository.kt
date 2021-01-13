c0bdd4bcbb05a6eddf70ffad4fc6f8add16f331e
manisha-khandare <manisha.khandare@gmail.com>
2021-01-13 10:36:50 +0530

    Created db table

---
+++ app/src/main/java/com/example/roomexapplication/WordRepository.kt
@@ -0,0 +1,10 @@
+package com.example.roomexapplication
+
+import android.app.Application
+import androidx.lifecycle.LiveData
+
+
+class WordRepository {
+
+
+}
---
+++ app/src/main/java/com/example/roomexapplication/WordRoomDatabase.kt
@@ -0,0 +1,5 @@
+package com.example.roomexapplication
+
+enum class WordRoomDatabase {
+
+}
